3e5ca2026362bf3579fe648532b8e54081d1b9b1
hawken <hawken@thehawken.org>
2014-10-01 14:29:50 +0200

    prettify config-example.py

--- config-example.py
+++ config-example.py
@@ -2,26 +2,22 @@
 
 import syslog
 
-UDP_IP = "192.168.0.1"                    # Your IP within your VPN/LAN
-# Source addresses to accept packets from
-# This should be limited to the addresses that also run blurouter.
-UDP_SUBNET = "192.168.0.0/24"
-UDP_BROADCAST = "192.168.0.255"           # Address all boxes will use to broadcast
-UDP_PORT=12345                            # Port all boxes will use to broadcast
+UDP_IP         = "192.168.0.1"         # Your IP within your VPN/LAN
+UDP_SUBNET     = "192.168.0.0/24"      # Addresses that we accept BluRouter packets from.
+UDP_BROADCAST  = "192.168.0.255"       # Address all boxes will use to broadcast
+UDP_PORT       = 12345                 # Port all boxes will use to broadcast
 
-ALLOW_RANGES    = ["192.168.0.0/16"]      # Entire VPN/LAN range you use
-PROTECTED_NETS  = ["192.168.0.0/24"]      # The VPN/LAN range this box is in
+ALLOW_RANGES   = ["192.168.0.0/16"]    # Entire VPN/LAN range you use
+PROTECTED_NETS = ["192.168.0.0/24"]    # The VPN/LAN range this box is in
 
-syslog_pri = syslog.LOG_NOTICE
-syslog_facil = syslog.LOG_USER
+syslog_pri     = syslog.LOG_NOTICE     # Syslog priority
+syslog_facil   = syslog.LOG_USER       # Syslog facility
 
-routesfile='/path/to/routes.txt'
-pidfile='/path/to/router.pid'
+routesfile     = '/path/to/routes.txt' # Must be absolute path. Contains the routes to share
+pidfile        = '/path/to/router.pid' # Must be absolute path.
 
-# little endian (x86 / amd64)
-endian=0
-# big endian
-#endian=1
+endian         = 0 # Little endian (amd64 / x86)
+#endian        = 1 # Big endian
 
 # Options you shouldn't touch unless you know what you are doing follows:
 
@@ -40,7 +36,7 @@ hello_interval = 30
 # if you do not tolerate losses,    set it between 1-2 intervals.
 # If you tolerate one lost packet,  set it between 2-3 intervals.
 # If you tolerate two lost packets, set it between 3-4 intervals.
-hello_timeout = 100
+hello_timeout  = 100
 
 # select_timeout:
 # Give up on select every second
@@ -59,7 +55,7 @@ select_timeout = 1
 # max_ttl:
 # This is how long someone can be offline without us assuming they are still online
 # We will never let someone say they can be silent for more than an hour
-max_ttl=3600
+max_ttl        = 3600
 
 # newip_sendnets:
 # Testing this behavior:
